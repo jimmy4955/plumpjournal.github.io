05e95bc86d1d01e6e0445890a3b4cb8682af3e86
PlumpJournal <103313595+jimmy4955@users.noreply.github.com>
2025-04-08 05:34:17 +0800

    for test

---
+++ image-list.js
@@ -0,0 +1,10 @@
+const imageList = [
+  "Alcreate/AI_BBW/bbw01.png",
+  "Alcreate/AI_BBW/bbw02.png",
+  "Alcreate/AI_BBW/bbw03.png",
+  "Alcreate/AI_BBW/bbw04.png",
+  "Alcreate/BeforeAndAfter/ba01.png",
+  "Alcreate/BeforeAndAfter/ba02.png",
+  "Alcreate/BeforeAndAfter/ba03.png",
+  "Alcreate/BeforeAndAfter/ba04.png",
+];
